364a7a77b508fb3b7a5c137020fb4c32e085bbb6
Javier Fernandez <javierfz1980@gmail.com>
2020-07-18 14:48:13 -0300

    Introduce Gallery layout

--- reddit/src/pages/Gallery/Gallery.tsx
+++ reddit/src/pages/Gallery/Gallery.tsx
@@ -1,5 +1,26 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
+import styles from './Gallery.module.scss';
+import { GalleryItem } from '../../components/GalleryItem/GalleryItem';
+import { NoContent } from '../../components/NoContent';
 
 export function Gallery() {
-  return <div>Favs</div>;
+  const gallery = useSelector(({ redditState }) => redditState.gallery);
+
+  if (!gallery.length) {
+    return (
+      <h1 className={styles.noConents}>
+        <NoContent />
+      </h1>
+    );
+  }
+
+  return (
+    <div className={styles.galleryItemsWrapper}>
+      {gallery?.map((image, idx) => (
+        <GalleryItem image={image} key={idx} />
+      ))}
+    </div>
+  );
 }
