23bebfbdc195e6dbf7843c366bc54fa6a783104d
Javier Fernandez <javierfz1980@gmail.com>
2020-07-18 13:17:44 -0300

    Introduce Router Navigation and styling

--- reddit/src/components/Header/Navigation/Naviagtion.tsx
+++ reddit/src/components/Header/Navigation/Naviagtion.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import { Badge, Nav } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
+import { useLocation, Link } from 'react-router-dom';
+
+import { Routes } from '../../../constants';
 
 export function Navigation() {
+  const favs = useSelector(({ redditState }) => redditState.favs);
+  const location = useLocation();
+
   return (
-    <Nav className="justify-content-end" activeKey="/home">
+    <Nav className="justify-content-end" activeKey={location.pathname}>
       <Nav.Item>
-        <Nav.Link href="/home">Home</Nav.Link>
+        <Nav.Link eventKey={Routes.home.path} as="div">
+          <Link to={Routes.home.path}>Home</Link>
+        </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link href="/favs">
-          Favs <Badge variant="info">9</Badge>
+        <Nav.Link eventKey={Routes.favs.path} as="div">
+          <Link to={Routes.favs.path}>
+            Favs <Badge variant="info">{favs?.length || 0}</Badge>
+          </Link>
         </Nav.Link>
       </Nav.Item>
     </Nav>
